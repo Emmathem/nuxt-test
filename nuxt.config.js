3e4bdff3daf261332a946920573219949cefbb57
Falua Temitope <emmat0616@gmail.com>
2022-03-26 17:49:53 +0100

    updated

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,10 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
+  ssr: false,
+  server: {
+    port: 3700,
+    host: '0.0.0.0',
+  },
   head: {
     title: 'nuxt-test',
     htmlAttrs: {
