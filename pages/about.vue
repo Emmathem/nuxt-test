863e4f770b3f9a2e0ec39aa55029313b8198a6c6
Falua Temitope <emmat0616@gmail.com>
2022-03-27 00:52:25 +0100

    new page

---
+++ pages/about.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div class="t-bg-blue-800 t-flex t-justify-center t-h-14 t-items-center">
+      <h3>About Me</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "about"
+}
+</script>
+
+<style scoped>
+
+</style>
