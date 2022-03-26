339c98bcbf66e394c9b7d1a7f6ff0ce88dc245d8
Falua Temitope <emmat0616@gmail.com>
2022-03-27 00:52:16 +0100

    gta added

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,11 @@
 <template>
-  <div class="t-h-screen">
-    <div class="t-flex t-justify-center t-p-5">Welcome here</div>
+  <div class="t-h-screen t-bg-black t-p-5">
+    <div class="t-border t-border-blue-100">
+      <div class="t-flex t-justify-center t-p-5">
+        <span class="t-text-blue-400">Welcome here</span>
+      </div>
+      <h3 class="t-text-blue-200"><nuxt-link to="/about">About Me</nuxt-link></h3>
+    </div>
   </div>
 </template>
 
